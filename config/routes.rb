b12b881f8a6393191d9dbc022dc0302ce16f68fb
Jesse Garrison <jmgarrison@gmail.com>
2015-06-21 22:43:25 -0700

    Fixes user resource

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :v1 do
     resource :sessions, only: [:create, :destroy]
-    resource :users, only: [:create]
+    resources :users, only: [:create]
   end
 
 end
